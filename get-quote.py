cc53a75b2e47595e77af97d98d47bd0e28989b50
Jayaram18 <kayaramsingh1999@gmail.com>
2021-02-25 17:49:03 +0530

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def quote():
+def randomquote():
    print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def quote():
   #print(quotes)
 
 if __name__== "__main__":
-  quote()
+  randomquote()
